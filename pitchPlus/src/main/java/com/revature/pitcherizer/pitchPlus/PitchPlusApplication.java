396ecc25ec6e24fc064c03db07ae7001be8c897f
Brian Conlon <brian.conlon18@myhunter.cuny.edu>
2021-01-25 15:22:42 -0500

    Spring boot skeleton; Database in progress. Mapping next.

---
+++ pitchPlus/src/main/java/com/revature/pitcherizer/pitchPlus/PitchPlusApplication.java
@@ -0,0 +1,13 @@
+package com.revature.pitcherizer.pitchPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchPlusApplication.class, args);
+	}
+
+}
---
+++ pitchPlus/src/test/java/com/revature/pitcherizer/pitchPlus/PitchPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.pitcherizer.pitchPlus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
