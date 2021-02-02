1f104579b9183ad8d4075a11ddfc79c80b327853
Jason Zelonka <jzelonka@ufl.edu>
2021-02-02 14:35:43 -0500

    added some frontend

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/app.module.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalhomeComponent } from './components/globalhome/globalhome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalhomeComponent
   ],
   imports: [
     BrowserModule,
