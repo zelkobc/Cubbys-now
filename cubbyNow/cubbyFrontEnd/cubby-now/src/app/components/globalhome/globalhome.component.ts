2d10362d8f1409f32df928e4a2b8f1ea755a9e19
Jason Zelonka <jzelonka@ufl.edu>
2021-02-02 15:15:37 -0500

    added some classes

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/components/globalhome/globalhome.component.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/components/globalhome/globalhome.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalhomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  public addPost()
+  {
+
   }
 
 }
