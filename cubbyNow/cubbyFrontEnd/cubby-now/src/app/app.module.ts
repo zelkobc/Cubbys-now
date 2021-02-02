7a8ad476709813247fffa79a6dbccc0390525c79
Jason Zelonka <jzelonka@ufl.edu>
2021-02-02 15:08:12 -0500

    built components

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/app.module.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalhomeComponent } from './components/globalhome/globalhome.component';
+import { LoginbarComponent } from './components/loginbar/loginbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GlobalhomeComponent
+    GlobalhomeComponent,
+    LoginbarComponent
   ],
   imports: [
     BrowserModule,
