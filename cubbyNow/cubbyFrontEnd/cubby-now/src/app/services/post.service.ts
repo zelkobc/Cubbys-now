d66975374e5609f6cfd24c6773c5fae2f4486d42
Jason Zelonka <jzelonka@ufl.edu>
2021-02-02 16:13:10 -0500

    added headers

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/services/post.service.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/services/post.service.ts
@@ -9,6 +9,8 @@ import { UrlService } from './url.service';
 export class PostService {
 
   url: string;
+  private formHeaders = this.urlServ.formHeaders;
+  private regHeaders = this.urlServ.regHeaders;
 
   constructor(private http: HttpClient, private urlServ: UrlService) {this.url = urlServ.getUrl() + "posts" }
 
@@ -17,7 +19,7 @@ export class PostService {
 
   addPost(post: Post)
   {
-    return this.http.post(this.url,JSON.stringify(post));
+    return this.http.post(this.url,JSON.stringify(post), {headers: this.regHeaders, withCredentials:true});
   }
 
   getPostById(id: number)
@@ -32,7 +34,7 @@ export class PostService {
 
   updatePost(post: Post)
   {
-    return this.http.put(this.url, post);
+    return this.http.put(this.url, post, {headers: this.regHeaders, withCredentials:true});
   }
 
   deletePostById(id: number)
