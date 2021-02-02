3b042b29c520c9c4c44708ad1b4456ca319a03f2
Jason Zelonka <jzelonka@ufl.edu>
2021-02-02 15:40:12 -0500

    post service written

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/components/globalhome/globalhome.component.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/components/globalhome/globalhome.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { UrlService } from 'src/app/services/url.service';
 
 @Component({
   selector: 'app-globalhome',
@@ -8,14 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlobalhomeComponent implements OnInit {
 
-  constructor(private http: HttpClient) { }
+  url: string;
+
+  constructor(private http: HttpClient, private urlServ: UrlService) {this.url = urlServ.getUrl() + "posts" }
 
   ngOnInit(): void {
   }
 
-  public addPost()
+  addPost(post: Post)
   {
+    return this.http.post(this.url,JSON.stringify(post));
+  }
 
+  getPostById(id: number)
+  {
+    return this.http.get(this.url + "/" + id);
+  }
+
+  getAllPosts()
+  {
+    return this.http.get(this.url + "/all");
+  }
+
+  updatePost(post: Post)
+  {
+    return this.http.put(this.url, post);
+  }
+
+  deletePostById(id: number)
+  {
+    return this.http.delete(this.url + "/" + id);
   }
 
 }
