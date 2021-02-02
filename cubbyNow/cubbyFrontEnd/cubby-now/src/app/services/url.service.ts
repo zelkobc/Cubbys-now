6ebf604d29f88bfabd64720cf3275651fd7f592d
Bobby <obbes240@gmail.com>
2021-02-02 15:23:34 -0500

    Url service and User service generated

---
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/services/url.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UrlService {
+
+  constructor() { }
+
+  getUrl(): string {
+    return 'http://localhost:8080/';
+  }
+}
