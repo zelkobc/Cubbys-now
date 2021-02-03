8749908a579b97f2dcd631cc000c3c4750b3b3c4
Jason Zelonka <jzelonka@ufl.edu>
2021-02-03 11:46:47 -0500

    groups work now

--- cubbyNow/cubbyBackEnd/src/main/java/com/revature/beans/Group.java
+++ cubbyNow/cubbyBackEnd/src/main/java/com/revature/beans/Group.java
@@ -1,15 +1,10 @@
 package com.revature.beans;
 
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -18,16 +13,11 @@ public class Group {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	@ManyToMany
-	@JoinTable(name = "groups_users", 
-    joinColumns = { @JoinColumn(name = "group_id") }, 
-    inverseJoinColumns = { @JoinColumn(name = "user_id") })
-	private Set<Integer> memberIds;
+	
 	
 	public Group()
 	{
 		id = -1;
-		memberIds = new HashSet<Integer>();
 	}
 
 	public Integer getId() {
@@ -38,20 +28,11 @@ public class Group {
 		this.id = id;
 	}
 
-	public Set<Integer> getMemberIds() {
-		return memberIds;
-	}
-
-	public void setMemberIds(Set<Integer> memberIds) {
-		this.memberIds = memberIds;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((memberIds == null) ? 0 : memberIds.hashCode());
 		return result;
 	}
 
@@ -69,18 +50,16 @@ public class Group {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (memberIds == null) {
-			if (other.memberIds != null)
-				return false;
-		} else if (!memberIds.equals(other.memberIds))
-			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Group [id=" + id + ", memberIds=" + memberIds + "]";
+		return "Group [id=" + id + "]";
 	}
+
+
+	
 	
 	
 }
