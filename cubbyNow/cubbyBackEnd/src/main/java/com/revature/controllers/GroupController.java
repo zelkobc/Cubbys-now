109e1dccffe2bf5c4ec9b25ac7d443d5ef0c47bc
Jason Zelonka <jzelonka@ufl.edu>
2021-02-03 15:05:18 -0500

    group backend done

--- cubbyNow/cubbyBackEnd/src/main/java/com/revature/controllers/GroupController.java
+++ cubbyNow/cubbyBackEnd/src/main/java/com/revature/controllers/GroupController.java
@@ -1,10 +1,14 @@
 package com.revature.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +47,29 @@ public class GroupController {
 		{
 			return ResponseEntity.ok(result);
 		}
-		return ResponseEntity.badRequest().build();
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping
+	public ResponseEntity<Void> updateGroup(@RequestBody Group group)
+	{
+		if (groupServ.updateGroup(group))
+		{
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Group>> getAllGroups()
+	{
+		return ResponseEntity.ok(groupServ.getAllGroups());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteGroup(@PathVariable Integer id)
+	{
+		groupServ.deleteGroupById(id);
+		return ResponseEntity.ok().build();
 	}
 }
--- cubbyNow/cubbyBackEnd/src/main/java/com/revature/services/GroupService.java
+++ cubbyNow/cubbyBackEnd/src/main/java/com/revature/services/GroupService.java
@@ -1,5 +1,7 @@
 package com.revature.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +27,25 @@ public class GroupService {
 	public Group getGroupById(Integer id)
 	{
 		return groupDAO.findById(id).get();
+	}
+	
+	public List<Group> getAllGroups()
+	{
+		return groupDAO.findAll();
+	}
+	
+	public boolean updateGroup(Group group)
+	{
+		if (groupDAO.existsById(group.getId()))
+		{
+			groupDAO.save(group);
+			return true;
+		}
+		return false;
+	}
+	
+	public void deleteGroupById(Integer id)
+	{
+		groupDAO.deleteById(id);
 	}
 }
