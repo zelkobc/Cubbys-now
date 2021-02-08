32baa667e8026eccf74f8530ef4a8bac51096fac
bhufnagel <Brodinator@Live.com>
2021-02-08 16:34:40 -0500

    Posts are now either public or affiliated with a group

--- cubbyNow/cubbyFrontEnd/cubby-now/src/app/models/post.ts
+++ cubbyNow/cubbyFrontEnd/cubby-now/src/app/models/post.ts
@@ -2,11 +2,13 @@ export class Post {
     postid: number;
     authorid: number;
     content: string;
+    groupid: number;
 
     constructor()
     {
         this.postid = -1;
         this.authorid = -1;
         this.content = "";
+        this.groupid = null;
     }
 }
